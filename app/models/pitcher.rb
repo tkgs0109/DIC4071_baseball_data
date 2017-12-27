4bc0d406fbe8bf6b329afd80571e724e2b48cef5
tkgs0109 <subtkgs@gmail.com>
2017-12-27 19:36:48 +0900

    Query8 is DONE

--- app/models/pitcher.rb
+++ app/models/pitcher.rb
@@ -4,12 +4,12 @@ class Pitcher < ApplicationRecord
     best_pitchers = pitchers.where(wins: max_win)
     best_pitchers.pluck(:player_id)
   end
-  
+
   def self.most_winners(year)
     pitchers = Pitcher.where(year_id: year)
     get_max_win_pitchers_id(pitchers)
   end
-  
+
   def self.top_ten(year)
     pitchers = Pitcher.where(year_id: year).order(wins: :desc)
     if pitchers.count < 10
@@ -20,14 +20,33 @@ class Pitcher < ApplicationRecord
     end
     best_pitchers.pluck(:player_id)
   end
-  
+
   def self.most_winners_in_team(year, team)
     pitchers = Pitcher.where(year_id: year, team_id: team)
     get_max_win_pitchers_id(pitchers)
   end
-  
+
   def self.most_winners_in_term(start_year, end_year)
     pitchers = Pitcher.where(year_id: start_year..end_year)
     get_max_win_pitchers_id(pitchers)
+  end
+
+  def self.best_deal(year)
+    pitchers = Pitcher.where(year_id: year)
+    cost_performance = 0
+    best_pitchers = []
+    pitchers.each do |pitcher|
+      if Salary.exists?(year_id: year, player_id: pitcher.player_id)
+        win = pitcher.wins
+        salary = Salary.find_by(year_id: year, player_id: pitcher.player_id).salary
+        if cost_performance < win/salary.to_f
+          cost_performance = win/salary.to_f
+          best_pitchers = [pitcher]
+        elsif cost_performance == win/salary.to_f
+          best_pitchers.push(pitcher)
+        end
+      end
+    end
+    best_pitchers.pluck(:player_id)
   end
 end
